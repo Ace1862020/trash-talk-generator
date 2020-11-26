437c4a7133c1dba23ea4f957c6a1aa1d8b2b674d
Ace1862020 <kyds1862015@gmail.com>
2020-11-27 02:07:15 +0800

    Add feature: Save the selected role.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const generateTrashTalk = require('./generate_trashtalk')
+const handlebars = require('handlebars')
 const app = express()
 const port = 3000
 
@@ -14,15 +15,27 @@ app.set('view engine', 'handlebars')
 // setting and to use body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// include helper
+handlebars.registerHelper('ifit', function (job, checkJob, options) {
+  if (job === checkJob) {
+    return options.fn(this)
+  }
+  return options.inverse(this)
+})
+
 // setting routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  const options = req.body
-  const trashTalk = generateTrashTalk(options)
-  res.render('index', { trashTalk: trashTalk, options: options })
+  // chosen staff
+  const roles = req.body.colleague
+  console.log(req.body.colleague)
+  console.log(req.body)
+  // correspond to the status of the render index 
+  const trashTalk = generateTrashTalk(roles)
+  res.render('index', { roles: roles, trashTalk: trashTalk })
 })
 
 // starts the express server and listening for connections
--- generate_trashtalk.js
+++ generate_trashtalk.js
@@ -1,11 +1,18 @@
 // random index
 function choose(array) {
   const index = Math.floor(Math.random() * array.length)
+  console.log('array', array)
   return array[index]
 }
 
-function generateTrashTalk(options) {
-  // 定義辭庫
+function generateTrashTalk(option) {
+  // 定義詞庫
+  const colleagueName = {
+    engineer: '身為一位工程師',
+    designer: '身為一位設計師',
+    entrepreneur: '身為一位創業家'
+  }
+
   const task = {
     engineer: ['加個按鈕', '新增功能', '切個版', '改一些code'],
     designer: ['畫一張圖', '改個logo', '順便設計一下', '隨便換個設計'],
@@ -13,28 +20,14 @@ function generateTrashTalk(options) {
   }
   const phrase = ['應該很簡單吧', '應該很容易吧', '還好而已吧', '算很正常吧']
 
-  // create collection
-  let collection = []
-  let colleagueName = ''
-  if (options.colleague === 'engineer') {
-    colleagueName = '身為一位工程師'
-    collection = collection.concat(task.engineer)
-  }
-  if (options.colleague === 'designer') {
-    colleagueName = '身為一位設計師'
-    collection = collection.concat(task.designer)
-  }
-  if (options.colleague === 'entrepreneur') {
-    colleagueName = '身為一位創業家'
-    collection = collection.concat(task.entrepreneur)
-  }
-  if (collection.length === 0) {
+  // Generate trash-talk
+  let trashTalk = ''
+
+  if (option === '' || option === null || option === undefined) {
     return '說點什麼吧!! 今天你一定要造點口業'
   }
 
-  // Generate trash-talk
-  let trashTalk = ''
-  trashTalk += `${colleagueName}，${choose(collection)}，${choose(phrase)}`
+  trashTalk += `${colleagueName[option]}，${choose(task[option])}，${choose(phrase)}`
 
   // return
   return trashTalk
